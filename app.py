8171960ed0b058626cce0274bae33fb31221cb5e
LuLu-89 <lucy.ly89@gmail.com>
2019-09-04 23:11:45 -0600

    Flask  works,  buttons do not

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect
 import pandas as pd
 from flask_pymongo import PyMongo
-# import scrape_mars 
+import dog_breed_predictor
 
 # Setup app
 app = Flask(__name__)
@@ -11,8 +11,6 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/dog_predictor_app")
 @app.route('/')
 def index ():
 
-    # mars = mongo.db.mars.find_one()
-    # return render_template('index.html', mars=mars)
     return render_template("index.html")
 
 @app.route('/predict')
@@ -27,16 +25,9 @@ def get_upload(filename):
 @app.route("/uploads/<path:filename>", methods=["POST"])
 def save_upload(filename):
     mongo.save_file(filename, request.files["file"])
-    return redirect("/predict", filename=filename))
+    return redirect("/predict", filename=filename)
 
 
-
-# @app.route('/scrape')
-# def scrape():
-#     mars = mongo.db.mars
-#     mars_data = scrape_mars.scrape()
-#     mars.update({}, mars_data, upsert=True)
-#     return redirect('/', code = 302)
 
 if __name__ == '__main__':
     app.run(debug=True)
