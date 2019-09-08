27a491b997f2c2fab82bac9aee5128924fd0c07f
Nate Anderson <theredpea@gmail.com>
2019-09-07 22:32:20 -0600

    Uploading an image file will POST to the upload route. The upload route will return JSON prediction results.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 import pandas as pd
 from flask_pymongo import PyMongo
 import dog_breed_predictor
@@ -28,13 +28,26 @@ def predict():
 
 #  Only need to handle POST requests here
 # -----------------------------------------------v
-@app.route("/uploads/<path:filename>", methods=["POST"])
-def save_upload(filename):
+@app.route("/upload", methods=["POST"])
+def save_upload():
+    # https://stackoverflow.com/a/46136495/1175496
+    # > Once you fetch the actual file with file = request.files['file'], you can get the filename with file.filename.
     # use same as the name attribute of the HTML <input>, which is "image"
-    # --------------------------------------v
-    mongo.save_file(filename, request.files["image"])
-    return redirect("/predict", code=302) 
+    # -----------------------------v
+    image_file = request.files["image"]
+    # mongo.save_file(image_file.filename, image_file)
+
+    # return redirect("/predict", code=302)
+    # er.py", line 273, in default
+    #     o.__class__.__name__)
+    # TypeError: Object of type float32 is not JSON serializable
+    # https://keras.io/applications/
+    # >  decode the results into a list of tuples (class, description, probability)
+    return pd.DataFrame(dog_breed_predictor.predict_from_file(image_file), columns=('class', 'description', 'probability')).to_json()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # https://github.com/jrosebr1/simple-keras-rest-api/issues/5#issuecomment-413461944
+    # Nate changed to avoid builtins.ValueError
+    # ValueError: Tensor Tensor("predictions/Softmax:0", shape=(?, 1000), dtype=float32) is not an element of this graph.
+    app.run(debug=False, threaded=False)
--- dog_breed_predictor.py
+++ dog_breed_predictor.py
@@ -6,35 +6,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import keras
 from keras.preprocessing import image
-from keras.applications.vgg19 import (
-    VGG19,
-    preprocess_input,
-    decode_predictions
-)
+from keras.applications.vgg19 import (VGG19, preprocess_input,
+                                      decode_predictions)
 import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg19 import (
-    VGG19,
-    preprocess_input,
-    decode_predictions
-)
+from tensorflow.keras.applications.vgg19 import (VGG19, preprocess_input,
+                                                 decode_predictions)
+from PIL import Image
+import io
+
+# Define image size
+
+#   File "C:\ProgramData\Anaconda3\lib\site-packages\matplotlib\image.py", line 646, in set_data
+#     raise TypeError("Invalid dimensions for image data")
+# TypeError: Invalid dimensions for image data
+image_size = (224, 224)
+
+# https://github.com/jrosebr1/simple-keras-rest-api/issues/5#issuecomment-413461944
+# Model
+model = VGG19(include_top=True, weights='imagenet')
 
 
 def dog_predictor():
 
-    # Define image size
-    image_size = (224, 224)
-
-
     # Bring in images
-    image_path = os.path.join("Images", "Affenpinscher", "Affenpinscher_233.jpg")
+    image_path = os.path.join("Images", "Affenpinscher",
+                              "Affenpinscher_233.jpg")
     img = image.load_img(image_path, target_size=image_size)
     plt.imshow(img)
-
-    # Model
-    model = VGG19(include_top=True, weights='imagenet')
 
     # Preprocess image for model prediction
     # This step handles scaling and normalization for VGG19
@@ -51,7 +52,6 @@ def dog_predictor():
 
     # Refactor above steps into reusable function
 
-
     def predict(image_path):
         """Use VGG19 to label image"""
         img = image.load_img(image_path, target_size=image_size)
@@ -61,7 +61,6 @@ def dog_predictor():
         predictions = model.predict(x)
         plt.imshow(img)
         print('Predicted:', decode_predictions(predictions, top=3))
-
 
     # Predicting some pure breeds to test model:
     image_path = os.path.join("dog_images_to_predict", "Boxer_69.jpg")
@@ -73,7 +72,8 @@ def dog_predictor():
     image_path = os.path.join("dog_images_to_predict", "Chihuahua_2973.jpg")
     predict(image_path)
 
-    image_path = os.path.join("dog_images_to_predict", "Labrador_retriever_209.jpg")
+    image_path = os.path.join("dog_images_to_predict",
+                              "Labrador_retriever_209.jpg")
     predict(image_path)
 
     # Using model to predict my dogs:
@@ -82,3 +82,35 @@ def dog_predictor():
 
     image_path = os.path.join("dog_images_to_predict", "Samson_07.jpg")
     predict(image_path)
+
+
+#the function must be at the top level, (aka dedented all the way to the left)
+#for you to use it in another file (like app.py)
+def predict_from_file(image_file):
+    # https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html
+    pil_image = image_file.read()
+    pil_image = Image.open(io.BytesIO(pil_image))
+    # if the image mode is not RGB, convert it
+    if pil_image.mode != "RGB":
+        pil_image = pil_image.convert("RGB")
+
+    # resize the input image and preprocess it
+    pil_image = pil_image.resize(image_size)
+    # print ('pil_image = image.img_to_array(pil_image)')
+    pil_image = image.img_to_array(pil_image)
+    # print('pil_image = np.expand_dims(pil_image, axis=0)')
+    pil_image = np.expand_dims(pil_image, axis=0)
+    # print('pil_image = preprocess_input(pil_image)')
+    pil_image = preprocess_input(pil_image)
+    # print('predictions = model.predict(pil_image)')
+    predictions = model.predict(pil_image)
+    # I don't think you want this code: `plt.imshow(img)`
+    
+    # File "C:\ProgramData\Anaconda3\lib\site-packages\matplotlib\image.py", line 646, in set_data
+    #     raise TypeError("Invalid dimensions for image data")
+    # TypeError: Invalid dimensions for image data
+    # 127.0.0.1 - - [07/Sep/2019 22:17:49] "POST /upload HTTP/1.1" 500 -
+    # plt.imshow(pil_image)
+    decoded_predictions = decode_predictions(predictions, top=3)
+    print('Predicted:', decoded_predictions)
+    return decoded_predictions
